01bbce8dd2f527b8f5b302c2f5e0c71a81bd0a32
jondjones <jon.jones.home@gmail.com>
2023-10-16 16:41:56 +0100

    instant

--- middleware.js
+++ middleware.js
@@ -1,34 +1,5 @@
 import { MiddlewareRequest } from '@netlify/next';
 
 export async function middleware(nextRequest) {
-
   return;
-  // if (nextRequest.nextUrl.pathname !== '/signup') {
-  //   return;
-  // }
-
-  // const middlewareRequest = new MiddlewareRequest(nextRequest);
-  // const response = await middlewareRequest.next();
-
-  // const GOOGLE_API_KEY = Deno.env.get('NEXT_PUBLIC_GOOGLE_API_KEY');
-  // const address = nextRequest.cookies.get('state');
-  // const country = nextRequest.cookies.get('country') ?? '';
-
-  // const api = new URL('https://maps.googleapis.com/maps/api/geocode/json');
-
-  // api.searchParams.set('address', `${address}${country}`);
-  // api.searchParams.set('key', GOOGLE_API_KEY);
-
-  // const res = await fetch(api.toString());
-  // const data = await res.json();
-  // const [result] = data.results;
-
-  // if (!result || !result.geometry || !result.geometry.location) {
-  //   return;
-  // }
-
-  // response.setPageProp('address', address);
-  // response.setPageProp('mapCords', result.geometry.location);
-
-  // return response;
 }
